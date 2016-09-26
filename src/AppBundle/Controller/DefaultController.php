a9f9b3bf0bada80ad45c052404b5d1037b454437
Amélie T <amelie.tamisier@gmail.com>
2016-09-26 17:14:20 +0200

    ajout donnee dans categories

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Categories;
 
 class DefaultController extends Controller
 {
@@ -18,5 +19,17 @@ class DefaultController extends Controller
         //replace this example code
         return $this->render('default/page.html.twig');
     }
+
+    public function createAction(){
+       $categories = new Categories();
+        $categories->setLibelle('Culture générale');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($categories);
+
+        $em->flush();
+    }
+
+
 
 }
